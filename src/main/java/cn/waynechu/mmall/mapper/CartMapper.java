3342778d85b61cb12fdac48ecf03b79998bdaf35
Wayne Chu <waynechu@waynechu.cn>
2018-05-21 19:35:24 +0800

    使用mybatis-generator-plugin生成entity及mapper

---
+++ src/main/java/cn/waynechu/mmall/mapper/CartMapper.java
@@ -0,0 +1,17 @@
+package cn.waynechu.mmall.mapper;
+
+import cn.waynechu.mmall.entity.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
